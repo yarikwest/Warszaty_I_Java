db4e4d9a74032315e0eec5ecb1856928fd5f9905
yarikwest <y.zakhidnyi@gmail.com>
2019-07-27 13:54:17 +0200

    Finish task 4

---
+++ src/Kostka_do_gry/Main.java
@@ -0,0 +1,37 @@
+package Kostka_do_gry;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(dropCubs("D12-1"));
+    }
+
+    static int dropCubs(String str) {
+        int result = 0;
+        int amount, cub, value;
+        Random random = new Random();
+        String[] tab = {"1", "0", "0"};
+        String[] drop = str.split("[D,+,-]");
+
+        for (int i = 0; i < drop.length; i++) {
+            if (!"".equals(drop[i])) {
+                tab[i] = drop[i].trim();
+            }
+        }
+        amount = Integer.parseInt(tab[0]);
+        cub = Integer.parseInt(tab[1]);
+        value = Integer.parseInt(tab[2]);
+
+        while (amount > 0) {
+            result += random.nextInt(cub)+1;
+            amount--;
+        }
+        if (str.contains("+"))
+            result += value;
+        else if (str.contains("-"))
+            result -= value;
+
+        return result;
+    }
+}
