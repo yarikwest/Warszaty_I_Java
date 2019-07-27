944dcd0a4f291c5cac10c1d09b73ececa66d24a3
yarikwest <y.zakhidnyi@gmail.com>
2019-07-27 16:06:48 +0200

    Finish task 5

---
+++ src/Wyszukiwarka_najpopularniejszych_slow/Main.java
@@ -0,0 +1,78 @@
+package Wyszukiwarka_najpopularniejszych_slow;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "http://www.onet.pl/";
+        String fileName = "popular_words.txt";
+        String filterFileName = "filtered_popular_words.txt";
+        Path path = getPath(fileName);
+        Path pathFilter = getPath(filterFileName);
+        String[] tab;
+        String[] filterTab = {"oraz", "ponieważ", "tylko", "znów", "cały", "jako"};
+
+        //write to "popular_words.txt"
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            for (Element elem : getTitles(url)) {
+                tab = elem.text().replaceAll("[^a-zA-Z0-9ąćęłńóśźż ]", "").split(" ");
+                for (String word : tab) {
+                    if (word.length() > 3) {
+                        writer.write(word + "\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //read from "popular_words.txt" and write to "filtered_popular_words.txt"
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+             BufferedWriter writer = Files.newBufferedWriter(pathFilter, StandardCharsets.UTF_8)) {
+
+            String str;
+            while ((str = reader.readLine()) != null) {
+                if (!Arrays.asList(filterTab).contains(str)) {
+                    writer.write(str + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Elements getTitles(String url) {
+        Connection connect = Jsoup.connect(url);
+        Elements links = null;
+        try {
+            Document document = connect.get();
+            links = document.select("span.title");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return links;
+    }
+
+    private static Path getPath(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        return path;
+    }
+
+}
